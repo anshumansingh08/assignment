534a73793f9ad0e4dafe9cf958507c267917b907
anshumansingh005 <anshuman.singh08@infosys.com>
2025-04-24 14:54:11 +0530

    Removed duplicate BrowserRouter

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,18 +1,18 @@
 import React from "react";
-import { BrowserRouter, Route, Routes as RouteData } from "react-router";
+import { Route, Routes as RouteData } from "react-router";
 import FormData from "./component/FormData";
 import NavBar from "./component/NavBar";
 import TableData from "./component/TableData";
 
 const Routes: React.FC = () => {
   return (
-    <BrowserRouter>
+    <>
       <NavBar />
       <RouteData>
         <Route path="/" element={<FormData />} />
         <Route path="/table" element={<TableData />} />
       </RouteData>
-    </BrowserRouter>
+    </>
   );
 };
 
