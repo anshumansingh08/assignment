33ce4b727cf0f6ec3f58fd71bd53fb0d27ce78ca
anshumansingh005 <anshuman.singh08@infosys.com>
2025-04-24 20:54:40 +0530

    Created Slice for the Form

---
+++ src/features/form/formSlice.ts
@@ -0,0 +1,54 @@
+import type { PayloadAction } from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
+export interface Applicant {
+  id: number;
+  name: string;
+  age: number | undefined;
+  email: string;
+  gender: "male" | "female" | "other";
+  techStack: string[];
+  hobbies: string;
+}
+interface ApplicantState {
+  applicants: Applicant[];
+}
+const initialState: ApplicantState = {
+  applicants: [],
+};
+let applicantId = 0;
+const formSlice = createSlice({
+  name: "applicant",
+  initialState,
+  reducers: {
+    addAplicant: (state, action: PayloadAction<Applicant>) => {
+      state.applicants.push({ ...action.payload, id: applicantId++ });
+    },
+    updateApplicant: (
+      state,
+      action: PayloadAction<{
+        id: number;
+        updatedApplicant: Omit<Applicant, "id">;
+      }>
+    ) => {
+      const { id, updatedApplicant } = action.payload;
+      const applicantIdx = state.applicants.findIndex((applicant) => {
+        applicant.id === id;
+      });
+      if (applicantIdx !== -1) {
+        state.applicants[applicantIdx] = { ...updatedApplicant, id: id };
+      }
+    },
+    deleteApplicant: (state, action: PayloadAction<number>) => {
+      const deleteId = action.payload;
+      state.applicants = state.applicants.filter((applicant) => {
+        applicant.id !== deleteId;
+      });
+    },
+  },
+});
+
+export const { addAplicant, updateApplicant, deleteApplicant } =
+  formSlice.actions;
+export const selectApplicants = (state: { applicant: ApplicantState }) =>
+  state.applicant.applicants;
+export default formSlice.reducer;
