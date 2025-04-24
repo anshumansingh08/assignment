69c200918fece4b5551ddc00957d2b53f94a7089
anshumansingh005 <anshuman.singh08@infosys.com>
2025-04-24 14:53:37 +0530

    code correction
    Importing Routes instead of NavBar component resolved issue of components not displaying to their respective link

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,13 @@ import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import "./App.css";
 import { store } from "./app/store";
-import NavBar from "./component/NavBar";
+import Routes from "./Routes";
 
 const App: React.FC = () => {
   return (
     <Provider store={store}>
       <BrowserRouter>
-        <NavBar />
+        <Routes />
       </BrowserRouter>
     </Provider>
   );
