209414df26cdd81c69b3d8995fd89b802aa90f08
anshumansingh005 <anshuman.singh08@infosys.com>
2025-04-25 12:17:18 +0530

    Typo fixed

--- src/app/store.ts
+++ src/app/store.ts
@@ -2,7 +2,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import applicantReducer from "../features/form/formSlice";
 export const store = configureStore({
   reducer: {
-    appicantData: applicantReducer,
+    applicant: applicantReducer,
   },
 });
 
