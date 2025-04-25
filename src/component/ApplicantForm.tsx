7cd68acf26d2ee492cc101beb7ae49aa51de2e1a
anshumansingh005 <anshuman.singh08@infosys.com>
2025-04-25 12:18:36 +0530

    Fixed errors due to type

--- src/component/ApplicantForm.tsx
+++ src/component/ApplicantForm.tsx
@@ -1,14 +1,13 @@
 import { Button, Checkbox, Form, Input, Radio } from "antd";
 import React, { useState } from "react";
-import { useDispatch } from "react-redux";
-import { Applicant, addAplicant } from "../features/form/formSlice";
+import { Applicant } from "../features/form/formSlice";
 
 interface ApplicantFormProps {
-  onApplicantAdded: (applicant: Applicant) => void; // Changed the prop to expect applicant data
+  onApplicantAdded: (applicant: Applicant) => void;
 }
 
 const ApplicantForm: React.FC<ApplicantFormProps> = ({ onApplicantAdded }) => {
-  const dispatch = useDispatch();
+  // const dispatch = useDispatch();
   const [form] = Form.useForm();
   const [formData, setFormData] = useState<Applicant>({
     id: 0,
@@ -32,6 +31,7 @@ const ApplicantForm: React.FC<ApplicantFormProps> = ({ onApplicantAdded }) => {
       | HTMLTextAreaElement
       | HTMLSelectElement
       | HTMLInputElement
+      | any
     >
   ) => {
     const { name, value } = e.target;
@@ -49,6 +49,11 @@ const ApplicantForm: React.FC<ApplicantFormProps> = ({ onApplicantAdded }) => {
       gender: e.target.value as Applicant["gender"],
     }));
     setErrors((prevErrors) => ({ ...prevErrors, gender: undefined }));
+  };
+
+  const handleTechStackChange = (checkedValues: string[]) => {
+    setFormData((prev) => ({ ...prev, techStack: checkedValues }));
+    setErrors((prevErrors) => ({ ...prevErrors, techStack: undefined }));
   };
 
   const validateForm = (): boolean => {
@@ -79,20 +84,23 @@ const ApplicantForm: React.FC<ApplicantFormProps> = ({ onApplicantAdded }) => {
       newErrors.hobbies = "Hobbies are required.";
       isValid = false;
     }
+    if (!formData.techStack || formData.techStack.length === 0) {
+      newErrors.techStack = "At least one technology must be selected.";
+      isValid = false;
+    }
 
     setErrors(newErrors);
     return isValid;
   };
 
-  const onFinish = (values: any) => {
+  const onFinish = () => {
     if (validateForm()) {
       const newApplicant: Applicant = {
-        ...values,
+        ...formData,
         id: 0,
       };
-      console.log("Form Data to be submitted:", newApplicant);
-      dispatch(addAplicant(newApplicant));
-      // form.resetFields();
+
+      form.resetFields();
       setFormData({
         id: 0,
         name: "",
@@ -109,107 +117,119 @@ const ApplicantForm: React.FC<ApplicantFormProps> = ({ onApplicantAdded }) => {
 
   return (
     <div className="container mx-auto p-4">
-      <h2 className="text-2xl font-bold mb-4 text-center">Applicant Form</h2>
-      <Form form={form} onFinish={onFinish} className="space-y-4">
-        <Form.Item
-          label="name"
-          name="name"
-          validateStatus={errors.name ? "error" : ""}
-          help={errors.name}
+      <div className="form justify-content-center">
+        {" "}
+        <h2 className="text-2xl font-bold mb-4 text-center">Applicant Form</h2>
+        <Form
+          labelCol={{ span: 8 }}
+          wrapperCol={{ span: 16 }}
+          form={form}
+          onFinish={onFinish}
+          className="space-y-4"
         >
-          <Input
-            placeholder="Enter your name"
+          <Form.Item
+            label="Name"
             name="name"
-            value={formData.name || ""}
-            onChange={handleChange}
-          />
-        </Form.Item>
-
-        <Form.Item
-          label="age"
-          name="age"
-          validateStatus={errors.age ? "error" : ""}
-          help={errors.age}
-        >
-          <Input
-            type="number"
-            placeholder="Enter your age"
-            name="age"
-            value={formData.age || ""}
-            onChange={handleChange}
-          />
-        </Form.Item>
-
-        <Form.Item
-          label="email"
-          name="email"
-          validateStatus={errors.email ? "error" : ""}
-          help={errors.email}
-        >
-          <Input
-            type="email"
-            placeholder="Enter your email"
-            name="email"
-            value={formData.email || ""}
-            onChange={handleChange}
-          />
-        </Form.Item>
-
-        <Form.Item
-          label="Gender"
-          name="gender"
-          validateStatus={errors.gender ? "error" : ""}
-          help={errors.gender}
-        >
-          <Radio.Group
-            defaultValue="male"
-            onChange={handleGenderChange}
-            value={formData.gender}
+            validateStatus={errors.name ? "error" : ""}
+            help={errors.name}
           >
-            <div className="flex items-center space-x-2">
-              <Radio value="male">Male</Radio>
-              <Radio value="female">Female</Radio>
-              <Radio value="other">Other</Radio>
-            </div>
-          </Radio.Group>
-        </Form.Item>
-        <Form.Item
-          label="Tech Stack"
-          name="techStack"
-          validateStatus={errors.techStack ? "error" : ""}
-          help={errors.techStack}
-        >
-          <Checkbox.Group onChange={handleChange} value={formData.techStack}>
-            <div className="flex flex-col">
-              <Checkbox value="javascript">JavaScript</Checkbox>
-              <Checkbox value="typescript">TypeScript</Checkbox>
-              <Checkbox value="react">React</Checkbox>
-              <Checkbox value="angular">Angular</Checkbox>
-              <Checkbox value="vue">Vue</Checkbox>
-              <Checkbox value="other">Other</Checkbox>
-            </div>
-          </Checkbox.Group>
-        </Form.Item>
-        <Form.Item
-          label="Hobbies"
-          name="hobbies"
-          validateStatus={errors.hobbies ? "error" : ""}
-          help={errors.hobbies}
-        >
-          <Input.TextArea
-            placeholder="Enter your hobbies"
-            name="hobbies"
-            value={formData.hobbies || ""}
-            onChange={handleChange}
-          />
-        </Form.Item>
+            <Input
+              placeholder="Enter your name"
+              name="name"
+              value={formData.name || ""}
+              onChange={handleChange}
+            />
+          </Form.Item>
 
-        <Form.Item>
-          <Button type="primary" htmlType="submit" className="w-full">
-            Add Applicant
-          </Button>
-        </Form.Item>
-      </Form>
+          <Form.Item
+            label="Age"
+            name="age"
+            validateStatus={errors.age ? "error" : ""}
+            help={errors.age}
+          >
+            <Input
+              type="number"
+              placeholder="Enter your age"
+              name="age"
+              value={formData.age || ""}
+              onChange={handleChange}
+            />
+          </Form.Item>
+
+          <Form.Item
+            label="Email"
+            name="email"
+            validateStatus={errors.email ? "error" : ""}
+            help={errors.email}
+          >
+            <Input
+              type="email"
+              placeholder="Enter your email"
+              name="email"
+              value={formData.email || ""}
+              onChange={handleChange}
+            />
+          </Form.Item>
+
+          <Form.Item
+            label="Gender"
+            name="gender"
+            validateStatus={errors.gender ? "error" : ""}
+            help={errors.gender}
+          >
+            <Radio.Group
+              defaultValue="male"
+              onChange={handleGenderChange}
+              value={formData.gender}
+            >
+              <div className="flex items-center space-x-2">
+                <Radio value="male">Male</Radio>
+                <Radio value="female">Female</Radio>
+                <Radio value="other">Other</Radio>
+              </div>
+            </Radio.Group>
+          </Form.Item>
+          <Form.Item
+            label="Tech Stack"
+            name="techStack"
+            validateStatus={errors.techStack ? "error" : ""}
+            help={errors.techStack}
+          >
+            <Checkbox.Group
+              onChange={handleTechStackChange}
+              value={formData.techStack}
+            >
+              <div className="flex flex-col">
+                <Checkbox value="javascript">JavaScript</Checkbox>
+                <Checkbox value="typescript">TypeScript</Checkbox>
+                <Checkbox value="react">React</Checkbox>
+                <Checkbox value="angular">Angular</Checkbox>
+                <Checkbox value="vue">Vue</Checkbox>
+                <Checkbox value="other">Other</Checkbox>
+              </div>
+            </Checkbox.Group>
+          </Form.Item>
+          <Form.Item
+            label="Hobbies"
+            name="hobbies"
+            validateStatus={errors.hobbies ? "error" : ""}
+            help={errors.hobbies}
+          >
+            <Input.TextArea
+              placeholder="Enter your hobbies"
+              name="hobbies"
+              value={formData.hobbies || ""}
+              onChange={handleChange}
+            />
+          </Form.Item>
+
+          <Form.Item className="formitem">
+            <Button type="primary" htmlType="submit" className="button">
+              Add Applicant
+            </Button>
+          </Form.Item>
+        </Form>
+      </div>
     </div>
   );
 };
