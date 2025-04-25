b9b01900ada31259990be55ebecc10be6128f724
anshumansingh005 <anshuman.singh08@infosys.com>
2025-04-25 09:36:00 +0530

    File renamed for naming convention

--- src/component/FormData.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const FormData: React.FC = () => {
-  return <div>FormData</div>;
-};
-
-export default FormData;
--- src/component/TableData.tsx
+++
@@ -1,5 +0,0 @@
-const Table: React.FC = () => {
-  return <div>Table</div>;
-};
-
-export default Table;
