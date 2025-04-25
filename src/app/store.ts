14972af597ec6b4d09c30aae4c36665e17ef25fb
anshumansingh005 <anshuman.singh08@infosys.com>
2025-04-25 09:37:27 +0530

    reducer renamed

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,7 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
+import applicantReducer from "../features/form/formSlice";
 export const store = configureStore({
   reducer: {
-    userData: userReducer,
+    appicantData: applicantReducer,
   },
 });
 
