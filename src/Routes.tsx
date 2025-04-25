5e0c7d4cdfc05f49241b0ea9889bcf61b504a4ad
anshumansingh005 <anshuman.singh08@infosys.com>
2025-04-25 09:36:41 +0530

    routes updated with props for the components

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,16 +1,30 @@
-import React from "react";
-import { Route, Routes as RouteData } from "react-router";
-import FormData from "./component/FormData";
+import React, { useState } from "react";
+import { Route, Routes as RouteData, useNavigate } from "react-router";
+import ApplicantForm from "./component/ApplicantForm";
+import ApplicantTable from "./component/ApplicantTable";
 import NavBar from "./component/NavBar";
-import TableData from "./component/TableData";
+import { Applicant } from "./features/form/formSlice";
 
 const Routes: React.FC = () => {
+  const [applicants, setApplicants] = useState<Applicant[]>([]);
+  const navigate = useNavigate();
+  const handleAddApplicant = (applicant: Applicant) => {
+    console.log(applicant);
+    setApplicants([...applicants, applicant]);
+    navigate("/table");
+  };
   return (
     <>
       <NavBar />
       <RouteData>
-        <Route path="/" element={<FormData />} />
-        <Route path="/table" element={<TableData />} />
+        <Route
+          path="/"
+          element={<ApplicantForm onApplicantAdded={handleAddApplicant} />}
+        />
+        <Route
+          path="/table"
+          element={<ApplicantTable applicants={applicants} />}
+        />
       </RouteData>
     </>
   );
